146648f8281254c7fcd7773a5a3958bcb7bba73b
Tucker <tuckerjamesmayo@gmail.com>
2015-09-07 12:13:23 -0700

    add Days controller and logic

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,56 @@
+class DaysController < ActionController::Base
+
+  def get_formatted_date
+    self.Date.try { |d| d.strftime(:my_format)}
+  end
+
+  def index
+    @day = Day.all
+  end
+
+  def new
+    @day = Day.new
+  end
+
+  def show
+    @day = Day.find(params[:id])
+  end
+
+  def edit
+    @day = Day.find(params[:id])
+  end
+
+  def create
+    @day = Day.new(day_params)
+    if @day.save
+      flash[:notice] = "Posted Sucessfully!!"
+       redirect_to day_path
+     else
+      render :new
+    end
+  end
+
+  def destroy
+    @day = Day.find(params[:id])
+    @day.destroy
+    flash[:notice] = "Smell you later, day."
+    redirect_to day_path
+  end
+
+  def update
+     @day = Day.find(params[:id])
+     if @day.update(params[:day])
+       redirect_to day_path
+     else
+       render :edit
+     end
+
+private
+
+  def day_params
+    params.require(:day).permit(:description, :date, :link_to)
+  end
+end
+
+
+end
---
+++ config/initializers/date_and_time_format.rb
@@ -0,0 +1,1 @@
+Time::DATE_FORMATS[:my_format] = '%m/%d/%y %l:%M%P' # 02/22/13 12:30pm
