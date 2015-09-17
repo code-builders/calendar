67a66773a09b59bfaea5ffa344a7fed83b8dc78b
Tucker <tuckerjamesmayo@gmail.com>
2015-09-17 13:58:39 -0700

    add scope to days.rb model to list lessons by date inputted, versus date created_by

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,15 +3,17 @@ class Day < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
   validates :description, :date, :url, :presence => true
+  default_scope -> { order(date: :asc) }
   has_many :activities
+
   include Searchable
   searchkick
   Day.import
-  def results
-    if @query.present?
-      self.class.search(@query).preload(:searchable).map!(&:searchable).uniq.sort_by{|e| -e.created_at.to_i}
-    else
-      Search.none
+    def results
+      if @query.present?
+        self.class.search(@query).preload(:searchable).map!(&:searchable).uniq.sort_by{|e| -e.created_at.to_i}
+      else
+        Search.none
+      end
     end
-  end
 end
