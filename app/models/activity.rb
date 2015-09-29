b88673b2f92ca52da92fa60af788c3a9857b2c6b
Bookis Smuin <bookis.smuin@gmail.com>
2015-09-29 15:36:48 -0700

    add correct ordering for date

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ActiveRecord::Base
-  default_scope -> { order("start asc") }
   belongs_to :day
   has_paper_trail
   validates :start, :end, :body, presence: true
+  scope :by_start, -> { order(start: :asc) }
 end
